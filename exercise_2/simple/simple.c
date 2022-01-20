6b16f1c83c59fd89838e4672f0c1eb6114c4ca99
Randy Kwalar <rkwalar@gmail.com>
2022-01-20 19:17:39 +0100

    Finish exercise 1 of project 2

--- exercise_2/simple/simple.c
+++ exercise_2/simple/simple.c
@@ -3,10 +3,14 @@
 #include <linux/module.h>
 #include <linux/hash.h>
 #include <linux/gcd.h>
+#include <asm/param.h>
+#include <linux/jiffies.h>
 
 int simple_init(void)
 {
     printk(KERN_INFO "The golden ratio is %llu ∖n", GOLDEN_RATIO_PRIME);
+    printk(KERN_INFO "The frequency of the timer interrupt is %u ∖n", HZ);
+    printk(KERN_INFO "The number of interrupts occurred since last boot is %lu ∖n", jiffies);
     printk(KERN_INFO "Loading Kernel Module\n");
 
     return 0;
@@ -15,6 +19,7 @@ int simple_init(void)
 void simple_exit(void)
 {
     printk(KERN_INFO "The gcd of 3, 300 and 24 is %lu\n", gcd(24, gcd(3, 300)));
+    printk(KERN_INFO "The number of interrupts occurred since last boot is %lu ∖n", jiffies);
     printk(KERN_INFO "Removing Kernel Module\n");
 }
 
