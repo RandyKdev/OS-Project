97444bc006e0a608fe81a58918c4070371ef8a1c
Randy Kwalar <rkwalar@gmail.com>
2022-01-22 08:05:00 +0100

    Completed exercise 1

--- exercise_1/shell.c
+++ exercise_1/shell.c
@@ -173,6 +173,67 @@ void prepare_redirect(char *args[], int redirect_location, int dup, int flag)
     shift_args_two_places_left(args, redirect_location + 2);
 }
 
+// copies what is after the pipe to args2 and frees it from args
+void prepare_pipe(char *args[], char *args2[], int pipe_location)
+{
+    int init_pipe = pipe_location;
+    pipe_location++;
+    for (int i = 0; args[pipe_location] != NULL && pipe_location < MAX_ARGS; pipe_location++, i++)
+    {
+        args2[i] = (char *)malloc((strlen(args[pipe_location]) + 1) * sizeof(char));
+        strcpy(args2[i], args[pipe_location]);
+    }
+
+    for (init_pipe; args[init_pipe] != NULL && init_pipe < MAX_ARGS; init_pipe++)
+    {
+        free(args[init_pipe]);
+        args[init_pipe] = NULL;
+    }
+}
+
+// executes pipe command
+void run_pipe_command(char *args[], int pipe_location)
+{
+    char *args2[MAX_ARGS] = {NULL};
+    prepare_pipe(args, args2, pipe_location);
+
+    int p[2];
+    if (pipe(p) < 0)
+    {
+        printf("Couldn't establish pipe\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // creates another child process
+    pid_t child_child_state = fork();
+
+    if (child_child_state == 0)
+    {
+        // child of child process
+
+        dup2(p[1], STDOUT_FILENO);
+        close(p[1]);
+
+        execvp(args[0], args);
+    }
+    else if (child_child_state < 0)
+    {
+        printf("osh> Child of child process couldn't be created\n");
+        exit(1);
+    }
+    else
+    {
+        // initial child process
+        wait(NULL);
+
+        dup2(p[0], STDIN_FILENO);
+        close(p[0]);
+
+        execvp(args2[0], args2);
+    }
+}
+
+// executes command in child process
 void execute_command_in_child_process(char *args[], char *history[])
 {
     // if user entered ! !, indicating history command
@@ -204,6 +265,14 @@ void execute_command_in_child_process(char *args[], char *history[])
     if (child_state == 0)
     {
         // child process
+
+        // checks if there is a pipe in args
+        int pipe_location = redirection(args, "|");
+        if (pipe_location)
+        {
+            run_pipe_command(args, pipe_location);
+            return;
+        }
 
         // Checks if there is output redirection
         int redirect_location = redirection(args, ">");
